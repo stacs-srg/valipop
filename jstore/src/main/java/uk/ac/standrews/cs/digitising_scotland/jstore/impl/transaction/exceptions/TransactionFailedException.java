c6b9e98c308144287c6ac4b7d3ea59c3e9e006e6
Alan Dearle <al@st-andrews.ac.uk>
2015-01-09 12:56:56 +0000

    Removed JStore as a module from digitising_scotland project JStore is now moved to quicksilver/hg/jstore updated JStore maven dependencies
    Former-commit-id: b0d9b857de0674139a387202b4377901fdc53280

--- jstore/src/main/java/uk/ac/standrews/cs/digitising_scotland/jstore/impl/transaction/exceptions/TransactionFailedException.java
+++
@@ -1,7 +0,0 @@
-package uk.ac.standrews.cs.digitising_scotland.jstore.impl.transaction.exceptions;
-
-/**
- * Created by al on 06/01/15.
- */
-public class TransactionFailedException extends Exception {
-}
