954874debefe111caafe68792855fd0f60745765
Graham Kirby <graham.kirby@st-andrews.ac.uk>
2015-06-15 21:53:11 +0100

    Fixed licence.
    Former-commit-id: d3112b7b99a6406b76154d635eefc4c6b53ca1e6

--- record_classification/src/main/java/uk/ac/standrews/cs/digitising_scotland/record_classification/classifier/StringSimilarityClassifier.java
+++ record_classification/src/main/java/uk/ac/standrews/cs/digitising_scotland/record_classification/classifier/StringSimilarityClassifier.java
@@ -1,13 +1,26 @@
+/*
+ * Copyright 2015 Digitising Scotland project:
+ * <http://digitisingscotland.cs.st-andrews.ac.uk/>
+ *
+ * This file is part of the module record_classification.
+ *
+ * record_classification is free software: you can redistribute it and/or modify it under the terms of the GNU General Public
+ * License as published by the Free Software Foundation, either version 3 of the License, or (at your option) any later
+ * version.
+ *
+ * record_classification is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied
+ * warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License along with record_classification. If not, see
+ * <http://www.gnu.org/licenses/>.
+ */
 package uk.ac.standrews.cs.digitising_scotland.record_classification.classifier;
 
 import uk.ac.shef.wit.simmetrics.similaritymetrics.InterfaceStringMetric;
-import uk.ac.standrews.cs.digitising_scotland.record_classification.model.Bucket;
 import uk.ac.standrews.cs.digitising_scotland.record_classification.model.Classification;
-import uk.ac.standrews.cs.digitising_scotland.record_classification.model.Record;
 
 import java.util.Map;
 import java.util.Objects;
-import java.util.concurrent.ConcurrentHashMap;
 
 /**
  * Classifies records based on the {@link InterfaceStringMetric similarity} of the training data to unseen data.
