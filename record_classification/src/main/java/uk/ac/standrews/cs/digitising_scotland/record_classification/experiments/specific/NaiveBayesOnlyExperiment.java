8cd356eca6343f19a691b820a8538780195bd543
Graham Kirby <graham.kirby@st-andrews.ac.uk>
2015-08-20 09:03:56 +0100

    Disabled Naive Bayes test.
    Former-commit-id: 842fcec444c0f4353d86a5006f1e2c4a33b4e0fb

--- record_classification/src/main/java/uk/ac/standrews/cs/digitising_scotland/record_classification/experiments/specific/NaiveBayesOnlyExperiment.java
+++ record_classification/src/main/java/uk/ac/standrews/cs/digitising_scotland/record_classification/experiments/specific/NaiveBayesOnlyExperiment.java
@@ -17,12 +17,10 @@
 package uk.ac.standrews.cs.digitising_scotland.record_classification.experiments.specific;
 
 import uk.ac.standrews.cs.digitising_scotland.record_classification.classifier.Classifier;
-import uk.ac.standrews.cs.digitising_scotland.record_classification.classifier.ClassifierSupplier;
 import uk.ac.standrews.cs.digitising_scotland.record_classification.exceptions.InputFileFormatException;
 import uk.ac.standrews.cs.digitising_scotland.record_classification.experiments.generic.Experiment;
 
 import java.io.IOException;
-import java.util.Collections;
 import java.util.List;
 import java.util.function.Supplier;
 
@@ -41,7 +39,7 @@ public class NaiveBayesOnlyExperiment extends Experiment {
 
     @Override
     protected List<Supplier<Classifier>> getClassifierFactories() throws IOException, InputFileFormatException {
-
-        return Collections.singletonList(ClassifierSupplier.NAIVE_BAYES);
+        return null;
+//        return Collections.singletonList(ClassifierSupplier.NAIVE_BAYES);
     }
 }
