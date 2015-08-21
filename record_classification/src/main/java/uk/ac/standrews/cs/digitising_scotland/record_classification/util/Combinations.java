0a481d902ca57921a03940af2bbf37c5362d43a0
Masih Hajiarabderkani <masih.h.derkani@st-andrews.ac.uk>
2015-08-21 13:41:50 +0100

    Fixed bugs in multiple classifier. Implemented multiple classifier experiment; work in progress.
    Former-commit-id: 89173c035d0eeaa48bab6e340ffdc558bb30f085

--- record_classification/src/main/java/uk/ac/standrews/cs/digitising_scotland/record_classification/util/Combinations.java
+++ record_classification/src/main/java/uk/ac/standrews/cs/digitising_scotland/record_classification/util/Combinations.java
@@ -25,12 +25,12 @@ public final class Combinations {
 
     private Combinations() { throw new UnsupportedOperationException(); }
 
-    public static <T> List<List<T>> permute(Collection<T> input) {
+    public static <T> List<List<T>> permutations(Collection<T> input) {
 
-        return permute(new ArrayList<>(input), 0);
+        return permutations(new ArrayList<>(input), 0);
     }
 
-    private static <T> List<List<T>> permute(List<T> input, int start) {
+    private static <T> List<List<T>> permutations(List<T> input, int start) {
 
         List<List<T>> permutations = new ArrayList<>();
 
@@ -44,7 +44,7 @@ public final class Combinations {
         else {
             for (int i = start; i < input.size(); i++) {
                 swap(input, start, i);
-                permutations.addAll(permute(input, start + 1));
+                permutations.addAll(permutations(input, start + 1));
                 swap(input, start, i);
             }
         }
@@ -54,8 +54,7 @@ public final class Combinations {
 
     private static <T> void swap(final List<T> a, final int start, final int i) {
 
-        T temp = a.get(start);
-
+        final T temp = a.get(start);
         a.set(start, a.get(i));
         a.set(i, temp);
     }
@@ -83,16 +82,14 @@ public final class Combinations {
 
     public static <T> List<List<T>> all(Collection<T> input) {
 
-        // TODO horrible code; implemnet properly
+        // TODO horrible code; implement properly
         final List<List<T>> powerset = powerset(input);
         final Set<List<T>> all = new HashSet<>(powerset);
 
         for (List<T> powerset_element : powerset) {
 
             if (powerset_element.size() > 1) {
-                List<T> reverse = new ArrayList<>(powerset_element);
-                Collections.reverse(reverse);
-                all.add(reverse);
+                all.addAll(permutations(powerset_element));
             }
         }
 
