6c0f8726ede36d7db744ce56e64a9f702dfd74c6
Masih Hajiarabderkani <masih.h.derkani@st-andrews.ac.uk>
2015-09-25 14:58:21 +0100

    Added test case for the potential bug in naive bayes classifier; this is a failing test. TO be discussed with Graham Fixed missing license headers
    Former-commit-id: 4ff0a3b2f5b904c30a8bb71a8a4667c75c0ab99c

---
+++ record_classification/src/test/java/uk/ac/standrews/cs/digitising_scotland/record_classification/classifier/naive_bayes/NaiveBayesClassifierTest.java
@@ -0,0 +1,42 @@
+/*
+ * Copyright 2015 Digitising Scotland project:
+ * <http://digitisingscotland.cs.st-andrews.ac.uk/>
+ *
+ * This file is part of the module record_classification.
+ *
+ * record_classification is free software: you can redistribute it and/or modify it under the terms of the GNU General Public
+ * License as published by the Free Software Foundation, either version 3 of the License, or (at your option) any later
+ * version.
+ *
+ * record_classification is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied
+ * warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License along with record_classification. If not, see
+ * <http://www.gnu.org/licenses/>.
+ */
+package uk.ac.standrews.cs.digitising_scotland.record_classification.classifier.naive_bayes;
+
+import org.junit.*;
+import uk.ac.standrews.cs.digitising_scotland.record_classification.model.*;
+
+import static org.junit.Assert.*;
+
+/**
+ * @author masih
+ */
+public class NaiveBayesClassifierTest {
+
+    @Test
+    public void testSpaceInClassificationCodeDuringTraining() throws Exception {
+
+        final NaiveBayesClassifier classifier = new NaiveBayesClassifier();
+
+        final Bucket gold_standard = new Bucket();
+
+        gold_standard.add(new Record(1, "fish", new Classification("swims", new TokenList("fish"), 1.0, null)));
+        gold_standard.add(new Record(2, "fish", new Classification("swims ", new TokenList("fish"), 1.0, null)));
+        
+        classifier.trainModel(gold_standard);
+
+    }
+}
