3d3db17fe594e597017d3ff265403d56c6abae5b
Masih Hajiarabderkani <masih.h.derkani@st-andrews.ac.uk>
2015-10-27 11:45:42 +0000

    Implemented load unseen command test. Refactored duplicate code in load records test into an abstract class.
    Former-commit-id: 95d6a56ac2dcac204917d067b465939ae63382ce

---
+++ record_classification/src/test/java/uk/ac/standrews/cs/digitising_scotland/record_classification/cli/command/LoadRecordsCommandTest.java
@@ -0,0 +1,168 @@
+package uk.ac.standrews.cs.digitising_scotland.record_classification.cli.command;
+
+import org.apache.commons.csv.*;
+import org.junit.*;
+import org.junit.runners.*;
+import uk.ac.standrews.cs.digitising_scotland.record_classification.cli.*;
+import uk.ac.standrews.cs.digitising_scotland.record_classification.model.*;
+
+import java.io.*;
+import java.nio.file.*;
+import java.util.*;
+
+import static org.junit.Assert.*;
+
+/**
+ * @author Masih Hajiarab Derkani
+ */
+public abstract class LoadRecordsCommandTest extends CommandTest {
+
+    protected static final Bucket TEST_RECORDS = new Bucket();
+    protected static final String ID = "ID";
+    protected static final String LABEL = "LABEL";
+    protected static final String CODE = "CODE";
+    protected static final CSVFormat DEFAULT_GOLD_STANDARD_FORMAT = CSVFormat.DEFAULT.withHeader(ID, LABEL, CODE);
+    protected static final Map<Integer[], CSVFormat> TEST_FORMATS = new HashMap<>();
+    protected static final List<Path> TEMP_PATHS = new ArrayList<>();
+
+    static {
+        TEST_RECORDS.add(new Record(1, "fish", new Classification("swim", new TokenList("fish"), 0.0, null)));
+        TEST_RECORDS.add(new Record(2, "dog", new Classification("bark", new TokenList("dog"), 0.0, null)));
+        TEST_RECORDS.add(new Record(3, "cat", new Classification("purr", new TokenList("cat"), 0.0, null)));
+        TEST_RECORDS.add(new Record(4, "shark", new Classification("swim", new TokenList("shark"), 0.0, null)));
+    }
+
+    static {
+        TEST_FORMATS.put(new Integer[]{0, 1, 2}, DEFAULT_GOLD_STANDARD_FORMAT);
+        TEST_FORMATS.put(new Integer[]{0, 1, 2}, CSVFormat.DEFAULT);
+        TEST_FORMATS.put(new Integer[]{0, 1, 2}, CSVFormat.RFC4180);
+        TEST_FORMATS.put(new Integer[]{0, 1, 2}, CSVFormat.EXCEL);
+        TEST_FORMATS.put(new Integer[]{0, 1, 2}, CSVFormat.MYSQL);
+        TEST_FORMATS.put(new Integer[]{0, 1, 2}, CSVFormat.TDF);
+        TEST_FORMATS.put(new Integer[]{0, 1, 2}, CsvFormatSupplier.RFC4180_PIPE_SEPARATED.get());
+        TEST_FORMATS.put(new Integer[]{0, 2, 1}, CSVFormat.DEFAULT.withHeader(ID, CODE, LABEL));
+        TEST_FORMATS.put(new Integer[]{2, 1, 0}, CSVFormat.DEFAULT.withHeader(CODE, LABEL, ID));
+    }
+
+    private Path path;
+    private CSVFormat format;
+    private int id_index;
+    private int label_index;
+
+    protected LoadRecordsCommandTest(Path path, CSVFormat format, int id_index, int label_index) {
+
+        this.path = path;
+        this.format = format;
+        this.id_index = id_index;
+        this.label_index = label_index;
+    }
+
+    protected Bucket getExpectedRecords() {
+
+        return TEST_RECORDS;
+    }
+
+    protected static Path printRecordsIntoTempFileWithFormat(Bucket records, CSVFormat format, int id_index, int label_index, int code_index) {
+
+        final Path temp_path = newTempFile();
+
+        try (final BufferedWriter out = Files.newBufferedWriter(temp_path)) {
+
+            final CSVPrinter printer = format.print(out);
+            records.stream().forEach(record -> {
+                try {
+
+                    final String[] record_csv = new String[3];
+                    record_csv[id_index] = String.valueOf(record.getId());
+                    record_csv[label_index] = record.getData();
+                    record_csv[code_index] = record.getClassification().getCode();
+                    printer.printRecord(record_csv);
+                }
+                catch (IOException e) { throw new IOError(e); }
+            });
+        }
+        catch (IOException e) { throw new IOError(e); }
+
+        return temp_path;
+    }
+
+    private static Path newTempFile() {
+
+        try {
+            return Files.createTempFile(null, null);
+        }
+        catch (IOException e) {
+            throw new IOError(e);
+        }
+    }
+
+    @AfterClass
+    public static void tearDownAfterClass() throws Exception {
+
+        TEMP_PATHS.forEach(LoadRecordsCommandTest::deleteQuietlyIfExists);
+    }
+
+    @Parameterized.Parameters(name = "{index} - id: {2}, label: {3}")
+    public static Collection<Object[]> data() {
+
+        final List<Object[]> arguments = new ArrayList<>();
+
+        TEST_FORMATS.entrySet().forEach(index_format -> {
+            final Integer[] indices = index_format.getKey();
+            final CSVFormat format = index_format.getValue();
+            final Integer id_index = indices[0];
+            final Integer label_index = indices[1];
+            final Integer code_index = indices[2];
+            final Path path = printRecordsIntoTempFileWithFormat(TEST_RECORDS, format, id_index, label_index, code_index);
+            arguments.add(new Object[]{path, format, id_index, label_index});
+            TEMP_PATHS.add(path);
+        });
+
+        return arguments;
+    }
+
+    protected List<Object> getArguments() {
+
+        final List<Object> arguments = new ArrayList<>();
+
+        arguments.add(LoadCommand.NAME);
+        arguments.add(LoadCommand.OPTION_SOURCE_SHORT);
+        arguments.add(path);
+        arguments.add(getSubCommandName());
+        arguments.add(LoadRecordsCommand.OPTION_DELIMITER_SHORT);
+        arguments.add(quote(format.getDelimiter()));
+        arguments.add(LoadRecordsCommand.OPTION_ID_COLUMN_INDEX_SHORT);
+        arguments.add(id_index);
+        arguments.add(LoadRecordsCommand.OPTION_LABEL_COLUMN_INDEX_SHORT);
+        arguments.add(label_index);
+
+        if (format.getHeader() != null) {
+            arguments.add(LoadRecordsCommand.OPTION_SKIP_HEADER_SHORT);
+        }
+
+        return arguments;
+    }
+
+    protected abstract String getSubCommandName();
+
+    @Test
+    public void testLoad() throws Exception {
+
+        run(getArguments().toArray());
+
+        final Bucket actual_records = getActualRecords();
+        assertEquals(getExpectedRecords(), actual_records);
+    }
+
+    protected abstract Bucket getActualRecords();
+
+    private static void deleteQuietlyIfExists(final Path path) {
+
+        try {
+            Files.deleteIfExists(path);
+        }
+        catch (IOException e) {
+            //skip
+        }
+    }
+}
