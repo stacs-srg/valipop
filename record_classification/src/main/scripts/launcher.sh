6eaaf1108291ebf83a2f6f03bab5de393b03ed50
Graham Kirby <graham.kirby@st-andrews.ac.uk>
2015-11-17 16:33:11 +0000

    Fixed broken licence header.
    Former-commit-id: c33e446a056e3f7e0252421beeaabb8413343f38

--- record_classification/src/main/scripts/launcher.sh
+++ record_classification/src/main/scripts/launcher.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-
+#
 # Copyright 2015 Digitising Scotland project:
 # <http://digitisingscotland.cs.st-andrews.ac.uk/>
 #
@@ -9,11 +9,12 @@
 # License as published by the Free Software Foundation, either version 3 of the License, or (at your option) any later
 # version.
 #
-# population_model is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied
+# record_classification is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied
 # warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for more details.
 #
-# You should have received a copy of the GNU General Public License along with population_model. If not, see
+# You should have received a copy of the GNU General Public License along with record_classification. If not, see
 # <http://www.gnu.org/licenses/>.
+#
 
 
 if [ -n "$1" ];
