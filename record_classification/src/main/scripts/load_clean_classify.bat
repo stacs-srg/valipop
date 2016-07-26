23fe6f71f31d9a5f5e72d50504f0653ac02fce71
Graham Kirby <graham.kirby@st-andrews.ac.uk>
2016-07-26 11:31:45 +0100

    Updated licence.
    Former-commit-id: 4a4f895633804d21f53e039c33ff1dfe9e213b5f

--- record_classification/src/main/scripts/load_clean_classify.bat
+++ record_classification/src/main/scripts/load_clean_classify.bat
@@ -1,7 +1,7 @@
 @REM
 @REM record_classification - Automatic record attribute classification.
-@REM Copyright © 2014-2016 Digitising Scotland project
-@REM (http://digitisingscotland.cs.st-andrews.ac.uk/)
+@REM Copyright © 2012-2016 Digitising Scotland project
+@REM (http://digitisingscotland.cs.st-andrews.ac.uk/record_classification/)
 @REM
 @REM This program is free software: you can redistribute it and/or modify
 @REM it under the terms of the GNU General Public License as published by
