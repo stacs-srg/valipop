cc7e4f2a40563a231d8ab401a3db9d7104acb9c5
Graham Kirby <graham.kirby@st-andrews.ac.uk>
2016-07-26 08:34:11 +0100

    Updated licence.
    Former-commit-id: 1773ed22b60b1bbd21a15654f0a14fbedf275ce1

--- record_classification/src/main/java/uk/ac/standrews/cs/digitising_scotland/record_classification/classifier/multiple_classifier/MultipleClassifier.java
+++ record_classification/src/main/java/uk/ac/standrews/cs/digitising_scotland/record_classification/classifier/multiple_classifier/MultipleClassifier.java
@@ -1,18 +1,20 @@
 /*
- * Copyright 2016 Digitising Scotland project:
- * <http://digitisingscotland.cs.st-andrews.ac.uk/>
+ * record_classification - Automatic record attribute classification.
+ * Copyright © 2014-2016 Digitising Scotland project
+ * (http://digitisingscotland.cs.st-andrews.ac.uk/)
  *
- * This file is part of the module record_classification.
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
  *
- * record_classification is free software: you can redistribute it and/or modify it under the terms of the GNU General Public
- * License as published by the Free Software Foundation, either version 3 of the License, or (at your option) any later
- * version.
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
  *
- * record_classification is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied
- * warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for more details.
- *
- * You should have received a copy of the GNU General Public License along with record_classification. If not, see
- * <http://www.gnu.org/licenses/>.
+ * You should have received a copy of the GNU General Public License
+ * along with this program. If not, see <http://www.gnu.org/licenses/>.
  */
 package uk.ac.standrews.cs.digitising_scotland.record_classification.classifier.multiple_classifier;
 
