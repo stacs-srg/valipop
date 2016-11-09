a76ecd386764da7d4950e5fcfa1de604bb2159d8
Tom Dalton <tsd4tsd4@st-andrews.ac.uk>
2016-11-09 14:50:57 +0000

    config files
    Former-commit-id: 718d4034985ae46abff24606fa4a721d85ca3f42

--- src/main/resources/scripts/run_model.sh
+++ src/main/resources/scripts/run_model.sh
@@ -24,15 +24,4 @@ then
 fi
 
 
-
-
-mvn exec:java -q -Dexec.cleanupDaemonThreads=false -Dexec.mainClass="model.simulationLogic.Simulation" -e -Dexec.args="$2"
-mvn exec:java -q -Dexec.cleanupDaemonThreads=false -Dexec.mainClass="model.simulationLogic.Simulation" -e -Dexec.args="$2"
-mvn exec:java -q -Dexec.cleanupDaemonThreads=false -Dexec.mainClass="model.simulationLogic.Simulation" -e -Dexec.args="$2"
-mvn exec:java -q -Dexec.cleanupDaemonThreads=false -Dexec.mainClass="model.simulationLogic.Simulation" -e -Dexec.args="$2"
-mvn exec:java -q -Dexec.cleanupDaemonThreads=false -Dexec.mainClass="model.simulationLogic.Simulation" -e -Dexec.args="$2"
-mvn exec:java -q -Dexec.cleanupDaemonThreads=false -Dexec.mainClass="model.simulationLogic.Simulation" -e -Dexec.args="$2"
-mvn exec:java -q -Dexec.cleanupDaemonThreads=false -Dexec.mainClass="model.simulationLogic.Simulation" -e -Dexec.args="$2"
-mvn exec:java -q -Dexec.cleanupDaemonThreads=false -Dexec.mainClass="model.simulationLogic.Simulation" -e -Dexec.args="$2"
-mvn exec:java -q -Dexec.cleanupDaemonThreads=false -Dexec.mainClass="model.simulationLogic.Simulation" -e -Dexec.args="$2"
-mvn exec:java -q -Dexec.cleanupDaemonThreads=false -Dexec.mainClass="model.simulationLogic.Simulation" -e -Dexec.args="$2"
+mvn exec:java -q -Dexec.cleanupDaemonThreads=false -Dexec.mainClass="model.simulationLogic.Simulation" -e -Dexec.args="$2 $3"
