fb95f2e893523912554a040dac430f2adccf4dbc
Alan Dearle <al@st-andrews.ac.uk>
2016-11-15 18:35:27 +0000

    Updated the BirthImporter and created KilmarnockLinker to call the 3 importers.
    Former-commit-id: 015c7a063b8190883cae6b87422a5f23cc8b62c4

--- linkage/src/main/java/uk/ac/standrews/cs/digitising_scotland/linkage/KilmarnockCommaSeparatedBirthImporter.java
+++ linkage/src/main/java/uk/ac/standrews/cs/digitising_scotland/linkage/KilmarnockCommaSeparatedBirthImporter.java
@@ -1,12 +1,10 @@
 package uk.ac.standrews.cs.digitising_scotland.linkage;
 
 import uk.ac.standrews.cs.digitising_scotland.linkage.lxp_records.Birth;
-import uk.ac.standrews.cs.digitising_scotland.linkage.lxp_records.Death;
-import uk.ac.standrews.cs.digitising_scotland.linkage.lxp_records.Marriage;
+import uk.ac.standrews.cs.digitising_scotland.util.ErrorHandling;
 import uk.ac.standrews.cs.storr.impl.exceptions.BucketException;
 import uk.ac.standrews.cs.storr.impl.exceptions.IllegalKeyException;
 import uk.ac.standrews.cs.storr.interfaces.IBucket;
-import uk.ac.standrews.cs.storr.interfaces.IReferenceType;
 import uk.ac.standrews.cs.util.dataset.DataSet;
 
 import java.io.IOException;
@@ -23,107 +21,31 @@ import java.util.List;
  * @author Graham Kirby (graham.kirby@st-andrews.ac.uk)
  */
 public class KilmarnockCommaSeparatedBirthImporter {
-
-    /**
-     * @param deaths        the bucket from which to import
-     * @param filename      containing the source records in digitising scotland format
-     * @param referencetype the expected type of the records being imported
-     * @return the number of records read in
-     * @throws IOException
-     * @throws RecordFormatException
-     * @throws BucketException
-     */
-    public static int importDigitisingScotlandDeaths(IBucket<Death> deaths, String filename, IReferenceType referencetype) throws RecordFormatException, IOException, BucketException, IllegalKeyException {
-        long counter = 0;
-//        try (final BufferedReader reader = Files.newBufferedReader(Paths.get(filename), FileManipulation.FILE_CHARSET)) {
-//
-//            int count = 0;
-//
-//            try {
-//                while (true) {
-//                    Death d = new Death();
-//                    importDigitisingScotlandRecord(d, reader, referencetype, DSFields.DEATH_FIELD_NAMES);
-//                    correctDeathFields(d);
-//                    try {
-//                        deaths.makePersistent(d);
-//                        count++;
-//                    } catch (BucketException e) {
-//                        ErrorHandling.exceptionError(e, "Error making death record persistent: " + d);
-//                    }
-//                }
-//            } catch (IOException e) {
-//                // expect this to be thrown when we getObjectById to the end.
-//            }
-//            return count;
-//        }
-        return 0;
-    }
-
-    /**
-     * @param marriages     the bucket from which to import
-     * @param filename      containing the source records in digitising scotland format
-     * @param referencetype the expected type of the records being imported
-     * @return the number of records read in
-     * @throws IOException
-     * @throws RecordFormatException
-     * @throws BucketException
-     */
-    public static int importDigitisingScotlandMarriages(IBucket<Marriage> marriages, String filename, IReferenceType referencetype, ArrayList<Long> oids) {
-        long counter = 0;
-//        try {
-//            try (final BufferedReader reader = Files.newBufferedReader(Paths.get(filename), FileManipulation.FILE_CHARSET)) {
-//
-//                int count = 0;
-//
-//                try {
-//                    while (true) {
-//                        Marriage m = new Marriage();
-//                        try {
-//                            importDigitisingScotlandRecord(m, reader, referencetype, DSFields.MARRIAGE_FIELD_NAMES);
-//                            correctMarriageFields(m);
-//
-//                        } catch (RecordFormatException e) {
-//                            ErrorHandling.exceptionError(e, "Record format error reading file: " + filename);
-//                        }
-//                        try {
-//                            marriages.makePersistent(m);
-//                            oids.add(m.getId());
-//                            count++;
-//                        } catch (Exception e) {
-//                            ErrorHandling.exceptionError(e, "Error making marriage record persistent: " + m);
-//                        }
-//                    }
-//                } catch (IOException e) {
-//                    // expect this to be thrown when we getObjectById to the end.
-//                }
-//                return count;
-//            }
-//        } catch (IOException e) {
-//            ErrorHandling.exceptionError(e, "Error opening buffered reader for file: " + filename);
-//            return 0;
-//        }
-        return 0;
-    }
-
+    
     /**
      * @param births        the bucket from which to import
      * @param filename      containing the source records in digitising scotland format
-     * @param referencetype the expected type of the records being imported
      * @return the number of records read in
      * @throws IOException
      * @throws RecordFormatException
      * @throws BucketException
      */
-    public static int importDigitisingScotlandBirths(IBucket<Birth> births, String filename, IReferenceType referencetype) throws RecordFormatException, IOException, BucketException, IllegalKeyException {
+    public static int importDigitisingScotlandBirths(IBucket<Birth> births, String filename, ArrayList<Long> oids) throws RecordFormatException, IOException, BucketException, IllegalKeyException {
 
         int count = 0;
-        DataSet data = new DataSet( Paths.get(filename) );
-        for( List<String> record : data.getRecords() ) {
-            Birth b = importDigitisingScotlandBirth( data, record );
+        DataSet data = new DataSet(Paths.get(filename));
+        for (List<String> record : data.getRecords()) {
+            Birth b = importDigitisingScotlandBirth(data, record);
+            try {
+                births.makePersistent(b);
+                oids.add(b.getId());
+                count++;
+            } catch (Exception e) {
+                ErrorHandling.exceptionError(e, "Error making birth record persistent: " + b);
+            }
         }
 
         return count;
-
     }
 
     /**
@@ -160,15 +82,6 @@ public class KilmarnockCommaSeparatedBirthImporter {
         System.out.println( b );
 
         return b;
-
-        // 	family	family beware	parents' marriage	date of birth	cs	cx	fs	fx	ms	mx	parmaryear	parmarplace
-        // line number	rd identifier	register identifier	entry no
-        // 	father's surname	father's occupation	mother's forename		mother's occupation
-        // year of parents' marriage	place of parent's marriage 1	place of parent's marriage 2	forename of informant	surname of informant
-        // relationship of informant to child	did informant  sign?	was informant present?	day of reg	month of reg	year of reg	illegitimate	notes	mother's previous married name
-        // address of informant	address of informant 2	address of informant 3
-        // declaration	fathers dom	fathers dom2	fathers dom3	mothers dom	mothers dom2	mothers dom3	edits	death	pid71	sch71
-
     }
 
     public static void main(String[] args) throws RecordFormatException, BucketException, IOException {
