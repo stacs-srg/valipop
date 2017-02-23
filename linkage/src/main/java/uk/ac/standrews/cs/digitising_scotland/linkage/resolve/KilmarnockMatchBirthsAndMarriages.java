c8307fc79fa66e44c29a7e5efefe0eeb15c9b1f3
Alan Dearle <al@st-andrews.ac.uk>
2017-02-23 17:18:32 +0000

    Changed interfaces of MTree to return distances
    Former-commit-id: 22ed3c1ae03f559e4dd887de1bbdcf2b93445482

--- linkage/src/main/java/uk/ac/standrews/cs/digitising_scotland/linkage/resolve/KilmarnockMatchBirthsAndMarriages.java
+++ linkage/src/main/java/uk/ac/standrews/cs/digitising_scotland/linkage/resolve/KilmarnockMatchBirthsAndMarriages.java
@@ -8,11 +8,9 @@ import uk.ac.standrews.cs.digitising_scotland.linkage.KilmarnockCommaSeparatedMa
 import uk.ac.standrews.cs.digitising_scotland.linkage.RecordFormatException;
 import uk.ac.standrews.cs.digitising_scotland.linkage.factory.*;
 import uk.ac.standrews.cs.digitising_scotland.linkage.lxp_records.*;
-import uk.ac.standrews.cs.digitising_scotland.util.ErrorHandling;
 import uk.ac.standrews.cs.digitising_scotland.util.MTree.DataDistance;
 import uk.ac.standrews.cs.digitising_scotland.util.MTree.Distance;
 import uk.ac.standrews.cs.digitising_scotland.util.MTree.MTree;
-import uk.ac.standrews.cs.storr.impl.LXP;
 import uk.ac.standrews.cs.storr.impl.StoreFactory;
 import uk.ac.standrews.cs.storr.impl.TypeFactory;
 import uk.ac.standrews.cs.storr.impl.exceptions.BucketException;
@@ -27,28 +25,19 @@ import java.util.ArrayList;
 import java.util.List;
 
 /**
- * Attempt to perform linking using MTree matching
+ * Output on std out matches between births and marriages in a csv format
+ * The output is the number of matches between births and marriages at edit distances 0,1,2.. RANGE_MAX
  * File is derived from KilmarnockLinker.
  * Created by al on 17/2/1017
  */
-public class KilmarnockMTreeMatcher {
+public class KilmarnockMatchBirthsAndMarriages {
 
     // Repositories and stores
 
     private static String input_repo_name = "BDM_repo";                             // input repository containing event records
-    private static String blocked_birth_repo_name = "blocked_birth_repo";           // repository for blocked Birth records
-    private static String FFNFLNMFNMMNPOMDOM_repo_name = "FFNFLNMFNMMNPOMDOM_repo";   // repository for blocked Marriage records
-    private static String FFNFLNMFNMMN_repo_name = "FFNFLNMFNMMN_repo";   // repository for blocked Marriage records
-
-    private static String linkage_repo_name = "linkage_repo";                       // repository for Relationship records
 
     private IStore store;
     private IRepository input_repo;             // Repository containing buckets of BDM records
-    private IRepository role_repo;
-    private IRepository blocked_births_repo;
-    private IRepository FFNFLNMFNMMNPOMDOM_repo;
-    private IRepository FFNFLNMFNMMN_repo;
-    private IRepository linkage_repo;
 
     // Bucket declarations
 
@@ -56,62 +45,47 @@ public class KilmarnockMTreeMatcher {
     private IBucket<Marriage> marriages;               // Bucket containing marriage records (inputs).
     private IBucket<Death> deaths;                     // Bucket containing death records (inputs).
 
-    private IBucket<Role> roles;                      // Bucket containing roles extracted from BDM records
-    private IBucket<Relationship> relationships;      // Bucket containing relationships between Roles
-
     // Paths to sources
 
     private static String births_name = "birth_records";                            // Name of bucket containing birth records (inputs).
     private static String marriages_name = "marriage_records";                      // Name of bucket containing marriage records (inputs).
     private static String deaths_name = "death_records";                            // Name of bucket containing marriage records (inputs).
 
-    // Names of buckets
-
-    private static String role_name = "roles";                                   // Name of bucket containing roles extracted from BDM records
-    private static String relationships_name = "relationships";                  // Name of bucket containing Relationship records
-
     private IReferenceType birthType;
     private IReferenceType deathType;
     private IReferenceType marriageType;
-    private IReferenceType roleType;
-    private IReferenceType relationshipType;
 
     private BirthFactory birthFactory;
     private DeathFactory deathFactory;
     private MarriageFactory marriageFactory;
-    private RoleFactory roleFactory;
-    private RelationshipFactory relationshipFactory;
     private ArrayList<Long> oids = new ArrayList<>();
     private int birth_count;
     private int death_count;
     private int marriage_count;
-    private int families_with_parents;
-    private int families_with_children;
-    private int single_children;
-    private int children_in_groups;
+
+    public final static int RANGE_MAX = 15;
 
     // Trees
 
     private  MTree<Marriage> marriageMtree;
 
-    public KilmarnockMTreeMatcher(String births_source_path, String deaths_source_path, String marriages_source_path) throws BucketException, RecordFormatException, IOException, RepositoryException, StoreException, JSONException {
+    public KilmarnockMatchBirthsAndMarriages(String births_source_path, String deaths_source_path, String marriages_source_path) throws BucketException, RecordFormatException, IOException, RepositoryException, StoreException, JSONException {
 
         System.out.println("Initialising");
         initialise();
 
         System.out.println("Ingesting");
         ingestBDMRecords(births_source_path, deaths_source_path, marriages_source_path);
-        //        checkBDMRecords();
 
         System.out.println("Creating Marriage MTree");
-        long time = System.currentTimeMillis();
         createMarriageMTreeOverGFNGLNBFNBMNPOMDOM();
-        long elapsed =  ( System.currentTimeMillis() - time ) / 1000 ;
-        System.out.println("Created Marriage MTree in " + elapsed + "s");
+        System.out.println("Results:");
+        System.out.println("------------");
 
-        formFamilies();
+        outputRangeSearchMatchesBetweenBirthsAndMarriages();
 
-        System.out.println("Finished");
+        System.out.println("------------");
+        System.out.println("End");
     }
 
     private void initialise() throws StoreException, IOException, RepositoryException, RecordFormatException, JSONException {
@@ -124,18 +98,14 @@ public class KilmarnockMTreeMatcher {
         System.out.println("Store path = " + store_path);
 
         input_repo = store.makeRepository(input_repo_name);
-        blocked_births_repo = store.makeRepository(blocked_birth_repo_name);  // a repo of Birth Buckets of records blocked by parents names, DOM, Place of Marriage.
-        FFNFLNMFNMMNPOMDOM_repo = store.makeRepository(FFNFLNMFNMMNPOMDOM_repo_name);  // a repo of Marriage Buckets
-        FFNFLNMFNMMN_repo = store.makeRepository(FFNFLNMFNMMN_repo_name);  // a repo of Marriage Buckets
 
-        linkage_repo = store.makeRepository(linkage_repo_name);
         initialiseTypes();
         initialiseFactories();
 
         births = input_repo.makeBucket(births_name, BucketKind.DIRECTORYBACKED, birthFactory);
         deaths = input_repo.makeBucket(deaths_name, BucketKind.DIRECTORYBACKED, deathFactory);
         marriages = input_repo.makeBucket(marriages_name, BucketKind.DIRECTORYBACKED, marriageFactory);
-        relationships = linkage_repo.makeBucket(relationships_name, BucketKind.DIRECTORYBACKED, relationshipFactory);
+
     }
 
     private void initialiseTypes() {
@@ -145,8 +115,6 @@ public class KilmarnockMTreeMatcher {
         birthType = tf.createType(Birth.class, "birth");
         deathType = tf.createType(Death.class, "death");
         marriageType = tf.createType(Marriage.class, "marriage");
-        roleType = tf.createType(Role.class, "role");
-        relationshipType = tf.createType(Relationship.class, "relationship");
     }
 
     private void initialiseFactories() {
@@ -154,8 +122,6 @@ public class KilmarnockMTreeMatcher {
         birthFactory = new BirthFactory(birthType.getId());
         deathFactory = new DeathFactory(deathType.getId());
         marriageFactory = new MarriageFactory(marriageType.getId());
-        roleFactory = new RoleFactory(roleType.getId());
-        relationshipFactory = new RelationshipFactory(relationshipType.getId());
     }
 
     /**
@@ -177,85 +143,9 @@ public class KilmarnockMTreeMatcher {
         marriage_count = KilmarnockCommaSeparatedMarriageImporter.importDigitisingScotlandMarriages(marriages, marriages_source_path, oids);
         System.out.println("Imported " + marriage_count + " marriage records");
 
-        // createRoles( births, deaths, marriages );
-    }
-
-    private void checkBDMRecords() {
-
-        System.out.println("Checking");
-        checkIngestedBirths();
-        checkIngestedDeaths();
-        checkIngestedMarriages();
-    }
-
-    private void checkIngestedBirths() {
-
-        IInputStream<Birth> stream = null;
-        try {
-            stream = births.getInputStream();
-        }
-        catch (BucketException e) {
-            ErrorHandling.exceptionError(e, "Cannot get stream for Births bucket");
-            return;
-        }
-
-        for (LXP l : stream) {
-            Birth birth_record = null;
-            try {
-                birth_record = (Birth) l;
-                System.out.println("Birth for: " + birth_record.get(Birth.FORENAME) + " " + birth_record.get(Birth.SURNAME) + " m: " + birth_record.get(Birth.MOTHERS_FORENAME) + " " + birth_record.get(Birth.MOTHERS_SURNAME) + " f: " + birth_record.get(Birth.FATHERS_FORENAME) + " " + birth_record
-                                .get(Birth.FATHERS_SURNAME) + " read OK");
-
-            }
-            catch (ClassCastException e) {
-                System.out.println("LXP found (not birth): oid: " + l.getId() + "object: " + l);
-                System.out.println("class of l: " + l.getClass().toString());
-            }
-        }
-    }
-
-    private void checkIngestedDeaths() {
-
-        IInputStream<Death> stream = null;
-        try {
-            stream = deaths.getInputStream();
-        }
-        catch (BucketException e) {
-            ErrorHandling.exceptionError(e, "Cannot get stream for Death bucket");
-            return;
-        }
-
-        System.out.println("Checking Deaths");
-
-        for (Death death_record : stream) {
-            System.out.println("Death for: " + death_record.get(Death.FORENAME) + " " + death_record.get(Death.SURNAME) + " m: " + death_record.get(Death.MOTHERS_FORENAME) + " " + death_record.get(Death.MOTHERS_SURNAME) + " f: " + death_record.get(Death.FATHERS_FORENAME) + " " + death_record
-                            .get(Death.FATHERS_SURNAME) + " read OK");
-        }
-    }
-
-    private void checkIngestedMarriages() {
-
-        IInputStream<Marriage> stream = null;
-        try {
-            stream = marriages.getInputStream();
-        }
-        catch (BucketException e) {
-            ErrorHandling.exceptionError(e, "Cannot get stream for Death bucket");
-            return;
-        }
-
-        System.out.println("Checking Marriages");
-
-        for (Marriage marriage_record : stream) {
-            System.out.println("Marriage for b: " + marriage_record.get(Marriage.BRIDE_FORENAME) + " " + marriage_record.get(Marriage.BRIDE_SURNAME) + " g: " + marriage_record.get(Marriage.GROOM_FORENAME) + " " + marriage_record.get(Marriage.GROOM_SURNAME));
-            System.out.println("\tbm: " + marriage_record.get(Marriage.BRIDE_MOTHERS_FORENAME) + " " + marriage_record.get(Marriage.BRIDE_MOTHERS_MAIDEN_SURNAME) + " bf: " + marriage_record.get(Marriage.BRIDE_FATHERS_FORENAME) + " " + marriage_record.get(Marriage.BRIDE_FATHERS_SURNAME));
-            System.out.println("\tgm: " + marriage_record.get(Marriage.GROOM_MOTHERS_FORENAME) + " " + marriage_record.get(Marriage.GROOM_MOTHERS_MAIDEN_SURNAME) + " gf: " + marriage_record.get(Marriage.GROOM_FATHERS_FORENAME) + " " + marriage_record.get(Marriage.GROOM_FATHERS_SURNAME));
-        }
     }
 
     private void createMarriageMTreeOverGFNGLNBFNBMNPOMDOM() throws RepositoryException, BucketException, IOException {
-
-        System.out.println("Creating M Tree of marriages by GFNGLNBFNBMNPOMDOMDistance...");
 
         marriageMtree = new MTree<Marriage>( new GFNGLNBFNBMNPOMDOMDistance() );
 
@@ -268,23 +158,10 @@ public class KilmarnockMTreeMatcher {
 
     }
 
-    private void printStats() {
-
-        System.out.println("Stats:");
-        System.out.println("Births: " + birth_count);
-        System.out.println("Deaths: " + death_count);
-        System.out.println("Marriages: " + marriage_count);
-
-        System.out.println("Parents in families: " + families_with_parents);
-        System.out.println("Sibling groups (>1 child) " + families_with_children);
-        System.out.println("Single children: " + single_children);
-
-    }
-
     /**
-     * Try and form families from Marriage M Tree data_array
+     * Output number of matches between births and marriages in csv format.
      */
-    private void formFamilies() {
+    private void outputRangeSearchMatchesBetweenBirthsAndMarriages() {
 
         IInputStream<Birth> stream;
         try {
@@ -307,23 +184,16 @@ public class KilmarnockMTreeMatcher {
             marriage_query.put( Marriage.MARRIAGE_MONTH, b.getString( Birth.PARENTS_MONTH_OF_MARRIAGE ) );
             marriage_query.put( Marriage.MARRIAGE_YEAR, b.getString( Birth.PARENTS_YEAR_OF_MARRIAGE ) );
 
-            List<DataDistance<Marriage>> results = marriageMtree.nearestN(marriage_query, 3);
+            for( int range = 0; range < RANGE_MAX;  ) {
 
-            print_result( results, b );
+                List<DataDistance<Marriage>> results = marriageMtree.rangeSearch(marriage_query, range++);
+                System.out.print( results.size() );
+                if( range != RANGE_MAX ) {
+                    System.out.print( "," );
+                }
 
-        }
-    }
-
-    private void print_result(List<DataDistance<Marriage>> results, Birth b) {
-        String parents = b.getFathersForename() + " " + b.getFathersSurname() + " + " + b.getMothersForename() + " " + b.getMothersMaidenSurname();
-        System.out.println( "child:                   " + b.get(Birth.FORENAME ) + " " + b.get(Birth.SURNAME ) + " p: " + parents + " at " + b.getPlaceOfMarriage() + " on " + b.getDateOfMarriage() );
-
-        for( DataDistance<Marriage> dd : results) {
-            Marriage m = dd.value;
-            String brideandgroom = m.getGroomsForename() + " " + m.getGroomsSurname() + " + " + m.getBridesForename() + " " + m.getBridesSurname();
-            String placedatemarraige = m.getPlaceOfMarriage() + " on " + m.getDateOfMarriage();
-
-            System.out.println("Marriage, distance = " + dd.distance + " marriage: " + brideandgroom + " at " + placedatemarraige);
+            }
+            System.out.println();
         }
     }
 
@@ -374,6 +244,6 @@ public class KilmarnockMTreeMatcher {
         String deaths_source_path = "/Digitising Scotland/KilmarnockBDM/deaths.csv";
         String marriages_source_path = "/Digitising Scotland/KilmarnockBDM/marriages.csv";
 
-        new KilmarnockMTreeMatcher(births_source_path, deaths_source_path, marriages_source_path);
+        new KilmarnockMatchBirthsAndMarriages(births_source_path, deaths_source_path, marriages_source_path);
     }
 }
