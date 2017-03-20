3215531fef93cce3b21d7d77eb736d2ea1f8d0b3
Özgür Akgün <ozgurakgun@gmail.com>
2017-03-20 18:03:42 +0000

    Run the jobs sequentially
    Former-commit-id: a726c0e574945df5c8aa7a28101e81100fd5c2d2

--- linkage/src/main/scripts/KilmarnockMTreeBirthBirthNNFamilyMergerSpaceExplorer.sh
+++ linkage/src/main/scripts/KilmarnockMTreeBirthBirthNNFamilyMergerSpaceExplorer.sh
@@ -21,17 +21,9 @@ FAMILY_SIZE=(5 10 20)
 MERGE_DISTANCES=(3 7 9 16)
 
 for distance in ${DISTANCES[@]}; do
-	for size in ${FAMILY_SIZE[@]}; do
-
-                # RUN 4 jobs in parallel at a time
-				CMD -e -Dexec.args="$FIXED_ARGS $distance $size 3" > /tmp/KilmarnockMTreeBirthBirthNNFamilyMergerSpaceExplorer${distance}${size}3.txt &
-				P1=$!
-				CMD -e -Dexec.args="$FIXED_ARGS $distance $size 7" > /tmp/KilmarnockMTreeBirthBirthNNFamilyMergerSpaceExplorer${distance}${size}7.txt &
-				P2=$!
-				CMD -e -Dexec.args="$FIXED_ARGS $distance $size 9" > /tmp/KilmarnockMTreeBirthBirthNNFamilyMergerSpaceExplorer${distance}${size}9.txt &
-				P3=$!
-				CMD -e -Dexec.args="$FIXED_ARGS $distance $size 16" > /tmp/KilmarnockMTreeBirthBirthNNFamilyMergerSpaceExplorer${distance}${size}16.txt &
-				P4=$!
-				wait $P1 $P2 $P3 $P4
-	done
+    for size in ${FAMILY_SIZE[@]}; do
+        for merge_distance in ${MERGE_DISTANCES[@]}; do
+            CMD -e -Dexec.args="$FIXED_ARGS $distance $size $merge_distance" > /tmp/KilmarnockMTreeBirthBirthNNFamilyMergerSpaceExplorer${distance}${size}${merge_distance}.txt
+        done
+    done
 done
