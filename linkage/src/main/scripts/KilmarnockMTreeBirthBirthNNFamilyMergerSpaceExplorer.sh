19501ba389217b3da1bebfd7c341df7c522dcff5
Özgür Akgün <ozgurakgun@gmail.com>
2017-03-21 20:43:14 +0000

    Use GNU Parallel to execute 4 jobs in parallel
    Former-commit-id: 81fce37ff9d59dcaa08b41daec8df4d3062153a8

--- linkage/src/main/scripts/KilmarnockMTreeBirthBirthNNFamilyMergerSpaceExplorer.sh
+++ linkage/src/main/scripts/KilmarnockMTreeBirthBirthNNFamilyMergerSpaceExplorer.sh
@@ -13,17 +13,15 @@ births="/DigitisingScotland/KilmarnockBDM/births.csv"
 deaths="/DigitisingScotland/KilmarnockBDM/deaths.csv"
 marriages="/DigitisingScotland/KilmarnockBDM/marriages.csv"
 
-alias CMD='mvn exec:java -q -Dexec.cleanupDaemonThreads=false -Dexec.mainClass="uk.ac.standrews.cs.digitising_scotland.linkage.resolve.KilmarnockMTreeBirthBirthNNFamilyMergerTruthChecker" '
 FIXED_ARGS="$births $deaths $marriages"
 
 DISTANCES=(3 7 9 16)
 FAMILY_SIZE=(5 10 20)
 MERGE_DISTANCES=(3 7 9 16)
 
-for distance in ${DISTANCES[@]}; do
-    for size in ${FAMILY_SIZE[@]}; do
-        for merge_distance in ${MERGE_DISTANCES[@]}; do
-            CMD -e -Dexec.args="$FIXED_ARGS $distance $size $merge_distance" > /tmp/KilmarnockMTreeBirthBirthNNFamilyMergerSpaceExplorer${distance}${size}${merge_distance}.txt
-        done
-    done
-done
+parallel --no-notice -j4 "mvn exec:java -q -Dexec.cleanupDaemonThreads=false -Dexec.mainClass='uk.ac.standrews.cs.digitising_scotland.linkage.resolve.KilmarnockMTreeBirthBirthNNFamilyMergerTruthChecker' -e -Dexec.args='$FIXED_ARGS {1} {2} {3}' > /tmp/KilmarnockMTreeBirthBirthNNFamilyMergerSpaceExplorer-{1}-{2}-{3}.txt" \
+    --joblog /tmp/KilmarnockMTreeBirthBirthNNFamilyMergerSpaceExplorer-joblog.txt \
+    ::: ${DISTANCES[@]} \
+    ::: ${FAMILY_SIZE[@]} \
+    ::: ${MERGE_DISTANCES[@]}
+
