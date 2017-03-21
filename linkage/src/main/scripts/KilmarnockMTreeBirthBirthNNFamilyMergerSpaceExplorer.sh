d8a5fd0f28d85001d6e4c846575acde2415c5d93
Özgür Akgün <ozgurakgun@gmail.com>
2017-03-21 20:59:52 +0000

    tidy up: extract mvn_args so we don't end up with a ridiculously long line
    Former-commit-id: 4b93b493e91d4b97c258bf6611abf2a7e1e2f387

--- linkage/src/main/scripts/KilmarnockMTreeBirthBirthNNFamilyMergerSpaceExplorer.sh
+++ linkage/src/main/scripts/KilmarnockMTreeBirthBirthNNFamilyMergerSpaceExplorer.sh
@@ -15,12 +15,15 @@ marriages="/DigitisingScotland/KilmarnockBDM/marriages.csv"
 
 FIXED_ARGS="$births $deaths $marriages"
 
+MVN_ARGS="-q -Dexec.cleanupDaemonThreads=false"
+MVN_ARGS="$MVN_ARGS -Dexec.mainClass='uk.ac.standrews.cs.digitising_scotland.linkage.resolve.KilmarnockMTreeBirthBirthNNFamilyMergerTruthChecker' -e"
+
 DISTANCES=(3 7 9 16)
 FAMILY_SIZE=(5 10 20)
 MERGE_DISTANCES=(3 7 9 16)
 
 parallel --no-notice -j4 --joblog /tmp/KilmarnockMTreeBirthBirthNNFamilyMergerSpaceExplorer-joblog.txt \
-    "mvn exec:java -q -Dexec.cleanupDaemonThreads=false -Dexec.mainClass='uk.ac.standrews.cs.digitising_scotland.linkage.resolve.KilmarnockMTreeBirthBirthNNFamilyMergerTruthChecker' -e -Dexec.args='$FIXED_ARGS {1} {2} {3}' > /tmp/KilmarnockMTreeBirthBirthNNFamilyMergerSpaceExplorer-{1}-{2}-{3}.txt" \
+    "mvn exec:java $MVN_ARGS -Dexec.args='$FIXED_ARGS {1} {2} {3}' > /tmp/KilmarnockMTreeBirthBirthNNFamilyMergerSpaceExplorer-{1}-{2}-{3}.txt" \
     ::: ${DISTANCES[@]} \
     ::: ${FAMILY_SIZE[@]} \
     ::: ${MERGE_DISTANCES[@]}
