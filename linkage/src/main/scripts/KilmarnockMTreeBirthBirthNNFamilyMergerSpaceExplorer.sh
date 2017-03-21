44acd8312889277f9842310ac2658e06b784272d
Özgür Akgün <ozgurakgun@gmail.com>
2017-03-21 20:55:05 +0000

    instant fix: the order of arguments seem to matter for gnu parallel
    Former-commit-id: d2e48303866ace411da4b7d09e27be9b16cee990

--- linkage/src/main/scripts/KilmarnockMTreeBirthBirthNNFamilyMergerSpaceExplorer.sh
+++ linkage/src/main/scripts/KilmarnockMTreeBirthBirthNNFamilyMergerSpaceExplorer.sh
@@ -19,8 +19,8 @@ DISTANCES=(3 7 9 16)
 FAMILY_SIZE=(5 10 20)
 MERGE_DISTANCES=(3 7 9 16)
 
-parallel --no-notice -j4 "mvn exec:java -q -Dexec.cleanupDaemonThreads=false -Dexec.mainClass='uk.ac.standrews.cs.digitising_scotland.linkage.resolve.KilmarnockMTreeBirthBirthNNFamilyMergerTruthChecker' -e -Dexec.args='$FIXED_ARGS {1} {2} {3}' > /tmp/KilmarnockMTreeBirthBirthNNFamilyMergerSpaceExplorer-{1}-{2}-{3}.txt" \
-    --joblog /tmp/KilmarnockMTreeBirthBirthNNFamilyMergerSpaceExplorer-joblog.txt \
+parallel --no-notice -j4 --joblog /tmp/KilmarnockMTreeBirthBirthNNFamilyMergerSpaceExplorer-joblog.txt \
+    "mvn exec:java -q -Dexec.cleanupDaemonThreads=false -Dexec.mainClass='uk.ac.standrews.cs.digitising_scotland.linkage.resolve.KilmarnockMTreeBirthBirthNNFamilyMergerTruthChecker' -e -Dexec.args='$FIXED_ARGS {1} {2} {3}' > /tmp/KilmarnockMTreeBirthBirthNNFamilyMergerSpaceExplorer-{1}-{2}-{3}.txt" \
     ::: ${DISTANCES[@]} \
     ::: ${FAMILY_SIZE[@]} \
     ::: ${MERGE_DISTANCES[@]}
