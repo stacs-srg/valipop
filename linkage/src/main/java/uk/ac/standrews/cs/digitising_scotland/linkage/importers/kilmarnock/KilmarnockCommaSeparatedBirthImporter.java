6d761c630137c5e49d21b6e4f0e53e663c45488f
Alan Dearle <al@st-andrews.ac.uk>
2017-03-22 11:04:01 +0000

    Sorting out post merge mayhem.
    Former-commit-id: cc419884a85a8c01e5e200d0ab7943142cd978a3

--- linkage/src/main/java/uk/ac/standrews/cs/digitising_scotland/linkage/importers/kilmarnock/KilmarnockCommaSeparatedBirthImporter.java
+++ linkage/src/main/java/uk/ac/standrews/cs/digitising_scotland/linkage/importers/kilmarnock/KilmarnockCommaSeparatedBirthImporter.java
@@ -1,16 +1,10 @@
 package uk.ac.standrews.cs.digitising_scotland.linkage.importers.kilmarnock;
 
-import uk.ac.standrews.cs.digitising_scotland.linkage.importers.RecordFormatException;
 import uk.ac.standrews.cs.digitising_scotland.linkage.importers.commaSeparated.CommaSeparatedBirthImporter;
 import uk.ac.standrews.cs.digitising_scotland.linkage.lxp_records.BirthFamilyGT;
 import uk.ac.standrews.cs.digitising_scotland.linkage.normalisation.DateNormalisation;
-import uk.ac.standrews.cs.storr.impl.exceptions.BucketException;
-import uk.ac.standrews.cs.storr.impl.exceptions.IllegalKeyException;
-import uk.ac.standrews.cs.storr.interfaces.IBucket;
 import uk.ac.standrews.cs.util.dataset.DataSet;
 
-import java.io.IOException;
-import java.nio.file.Paths;
 import java.util.List;
 
 import static uk.ac.standrews.cs.digitising_scotland.linkage.lxp_records.BirthFamilyGT.*;
@@ -90,48 +84,9 @@ public class KilmarnockCommaSeparatedBirthImporter extends CommaSeparatedBirthIm
         return RECORD_LABEL_MAP;
     }
 
-    /**
-     * @param births   the bucket from which to import
-     * @param births_source_path containing the source records in digitising scotland format
-     * @return the number of records read in
-     * @throws IOException
-     * @throws RecordFormatException
-     * @throws BucketException
-     */
-    public int importDigitisingScotlandBirths(IBucket<BirthFamilyGT> births, String births_source_path) throws IOException, RecordFormatException, BucketException {
-
-        DataSet data = new DataSet(Paths.get(births_source_path));
-        int count = 0;
-
-        for (List<String> record : data.getRecords()) {
-
-            BirthFamilyGT birth_record = importDigitisingScotlandBirth(data, record);
-            births.makePersistent(birth_record);
-            count++;
-        }
-
-        return count;
-    }
-
-
     @Override
     public String[] get_unavailable_records() {
         return UNAVAILABLE_RECORD_LABELS;
-    }
-
-    /**
-     * Fills in a record.
-     */
-    private BirthFamilyGT importDigitisingScotlandBirth(DataSet data, List<String> record) throws IOException, RecordFormatException, IllegalKeyException {
-
-        BirthFamilyGT birth = new BirthFamilyGT();
-
-        addAvailableSingleFields(data, record, birth, RECORD_LABEL_MAP);
-        addAvailableNormalisedFields(data, record, birth);
-        addAvailableCompoundFields(data, record, birth);
-        addUnavailableFields(birth, UNAVAILABLE_RECORD_LABELS);
-
-        return birth;
     }
 
     @Override
