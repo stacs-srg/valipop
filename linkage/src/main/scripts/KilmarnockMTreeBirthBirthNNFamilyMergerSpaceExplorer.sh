959e04aa33a7c18409e488c82d827b50de584ae4
Özgür Akgün <ozgurakgun@gmail.com>
2017-03-25 10:00:24 +0000

    Set Java's tmpdir to be /data/digitising_scotland_tmp -- or else we run out of disk space whilst I am sleeping!
    Former-commit-id: fdc7a20b219718a179e44eb48e5308c768069d81

--- linkage/src/main/scripts/KilmarnockMTreeBirthBirthNNFamilyMergerSpaceExplorer.sh
+++ linkage/src/main/scripts/KilmarnockMTreeBirthBirthNNFamilyMergerSpaceExplorer.sh
@@ -25,6 +25,11 @@ MERGE_DISTANCES=(3 7 9 16)
 # become root
 # mkdir /data/digitising_scotland_outputs
 # chown -R secure:secure /data/digitising_scotland_outputs
+
+# do the same with /data/digitising_scotland_tmp
+# and set the tmpdir to this directory. otherwise we risk running out of disk space with /tmp
+export _JAVA_OPTIONS=-Djava.io.tmpdir=/data/digitising_scotland_tmp
+
 parallel --no-notice -j4 --joblog /data/digitising_scotland_outputs/KilmarnockMTreeBirthBirthNNFamilyMergerSpaceExplorer-joblog.txt \
     --eta \
     "mvn exec:java $MVN_ARGS -Dexec.args='$FIXED_ARGS {1} {2} {3}' > /data/digitising_scotland_outputs/KilmarnockMTreeBirthBirthNNFamilyMergerSpaceExplorer-{1}-{2}-{3}.txt" \
