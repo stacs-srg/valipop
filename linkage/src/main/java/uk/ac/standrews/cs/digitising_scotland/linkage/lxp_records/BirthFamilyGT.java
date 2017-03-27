df68be4095dc3899c4cd3ce6f2bad589c7e1bd8a
Graham Kirby <graham.kirby@st-andrews.ac.uk>
2017-03-27 11:31:44 +0100

    Partially removed field duplication.
    Former-commit-id: ddcf9830745a424e1e4f29ba3c517728b75cb706

--- linkage/src/main/java/uk/ac/standrews/cs/digitising_scotland/linkage/lxp_records/BirthFamilyGT.java
+++ linkage/src/main/java/uk/ac/standrews/cs/digitising_scotland/linkage/lxp_records/BirthFamilyGT.java
@@ -15,102 +15,103 @@ import uk.ac.standrews.cs.storr.types.LXP_SCALAR;
  */
 public class BirthFamilyGT extends Birth {
 
-    @LXP_SCALAR(type = LXPBaseType.STRING)
-    public static final String ORIGINAL_ID = "ORIGINAL_ID";
+    // Fields need to be duplicated for reflective use to work.
 
     @LXP_SCALAR(type = LXPBaseType.STRING)
-    public static final String FORENAME = "forename";
+    public static final String ORIGINAL_ID = Birth.ORIGINAL_ID;
 
     @LXP_SCALAR(type = LXPBaseType.STRING)
-    public static final String CHANGED_FORENAME = "changed_forename";
+    public static final String FORENAME = Birth.FORENAME;
 
     @LXP_SCALAR(type = LXPBaseType.STRING)
-    public static final String SURNAME = "surname";
+    public static final String CHANGED_FORENAME = Birth.CHANGED_FORENAME;
 
     @LXP_SCALAR(type = LXPBaseType.STRING)
-    public static final String CHANGED_SURNAME = "changed_surname";
+    public static final String SURNAME = Birth.SURNAME;
 
     @LXP_SCALAR(type = LXPBaseType.STRING)
-    public static final String SEX = "sex";
+    public static final String CHANGED_SURNAME = Birth.CHANGED_SURNAME;
 
     @LXP_SCALAR(type = LXPBaseType.STRING)
-    public static final String FATHERS_FORENAME = "fathers_forename";
+    public static final String SEX = Birth.SEX;
 
     @LXP_SCALAR(type = LXPBaseType.STRING)
-    public static final String FATHERS_SURNAME = "fathers_surname";
+    public static final String FATHERS_FORENAME = Birth.FATHERS_FORENAME;
 
     @LXP_SCALAR(type = LXPBaseType.STRING)
-    public static final String MOTHERS_FORENAME = "mothers_forename";
+    public static final String FATHERS_SURNAME = Birth.FATHERS_SURNAME;
 
     @LXP_SCALAR(type = LXPBaseType.STRING)
-    public static final String MOTHERS_SURNAME = "mothers_surname";
+    public static final String MOTHERS_FORENAME = Birth.MOTHERS_FORENAME;
 
     @LXP_SCALAR(type = LXPBaseType.STRING)
-    public static final String MOTHERS_MAIDEN_SURNAME = "mothers_maiden_surname";
+    public static final String MOTHERS_SURNAME = Birth.MOTHERS_SURNAME;
 
     @LXP_SCALAR(type = LXPBaseType.STRING)
-    public static final String CHANGED_MOTHERS_MAIDEN_SURNAME = "changed_mothers_maiden_surname";
+    public static final String MOTHERS_MAIDEN_SURNAME = Birth.MOTHERS_MAIDEN_SURNAME;
 
     @LXP_SCALAR(type = LXPBaseType.STRING)
-    public static final String FATHERS_OCCUPATION = "fathers_occupation";
+    public static final String CHANGED_MOTHERS_MAIDEN_SURNAME = Birth.CHANGED_MOTHERS_MAIDEN_SURNAME;
 
     @LXP_SCALAR(type = LXPBaseType.STRING)
-    public static final String YEAR_OF_REGISTRATION = "YEAR_OF_REGISTRATION";
+    public static final String FATHERS_OCCUPATION = Birth.FATHERS_OCCUPATION;
 
     @LXP_SCALAR(type = LXPBaseType.STRING)
-    public static final String ENTRY = "ENTRY";
+    public static final String YEAR_OF_REGISTRATION = Birth.YEAR_OF_REGISTRATION;
 
     @LXP_SCALAR(type = LXPBaseType.STRING)
-    public static final String REGISTRATION_DISTRICT_SUFFIX = "REGISTRATION_DISTRICT_SUFFIX";
+    public static final String ENTRY = Birth.ENTRY;
 
     @LXP_SCALAR(type = LXPBaseType.STRING)
-    public static final String REGISTRATION_DISTRICT_NUMBER = "REGISTRATION_DISTRICT_NUMBER";
+    public static final String REGISTRATION_DISTRICT_SUFFIX = Birth.REGISTRATION_DISTRICT_SUFFIX;
 
     @LXP_SCALAR(type = LXPBaseType.STRING)
-    public static final String CORRECTED_ENTRY = "corrected_entry";
+    public static final String REGISTRATION_DISTRICT_NUMBER = Birth.REGISTRATION_DISTRICT_NUMBER;
 
     @LXP_SCALAR(type = LXPBaseType.STRING)
-    public static final String IMAGE_QUALITY = "image_quality";
+    public static final String CORRECTED_ENTRY = Birth.CORRECTED_ENTRY;
 
     @LXP_SCALAR(type = LXPBaseType.STRING)
-    public static final String BIRTH_DAY = "birth_day";
+    public static final String IMAGE_QUALITY = Birth.IMAGE_QUALITY;
 
     @LXP_SCALAR(type = LXPBaseType.STRING)
-    public static final String BIRTH_MONTH = "birth_month";
+    public static final String BIRTH_DAY = Birth.BIRTH_DAY;
 
     @LXP_SCALAR(type = LXPBaseType.STRING)
-    public static final String BIRTH_YEAR = "birth_year";
+    public static final String BIRTH_MONTH = Birth.BIRTH_MONTH;
 
     @LXP_SCALAR(type = LXPBaseType.STRING)
-    public static final String BIRTH_ADDRESS = "birth_address";
+    public static final String BIRTH_YEAR = Birth.BIRTH_YEAR;
 
     @LXP_SCALAR(type = LXPBaseType.STRING)
-    public static final String ILLEGITIMATE_INDICATOR = "illegitimate_indicator";
+    public static final String BIRTH_ADDRESS = Birth.BIRTH_ADDRESS;
 
     @LXP_SCALAR(type = LXPBaseType.STRING)
-    public static final String ADOPTION = "adoption";
+    public static final String ILLEGITIMATE_INDICATOR = Birth.ILLEGITIMATE_INDICATOR;
 
     @LXP_SCALAR(type = LXPBaseType.STRING)
-    public static final String PARENTS_DAY_OF_MARRIAGE = "parents_day_of_marriage";
+    public static final String ADOPTION = Birth.ADOPTION;
 
     @LXP_SCALAR(type = LXPBaseType.STRING)
-    public static final String PARENTS_MONTH_OF_MARRIAGE = "parents_month_of_marriage";
+    public static final String PARENTS_DAY_OF_MARRIAGE = Birth.PARENTS_DAY_OF_MARRIAGE;
 
     @LXP_SCALAR(type = LXPBaseType.STRING)
-    public static final String PARENTS_YEAR_OF_MARRIAGE = "parents_year_of_marriage";
+    public static final String PARENTS_MONTH_OF_MARRIAGE = Birth.PARENTS_MONTH_OF_MARRIAGE;
 
     @LXP_SCALAR(type = LXPBaseType.STRING)
-    public static final String PARENTS_PLACE_OF_MARRIAGE = "parents_place_of_marriage";
+    public static final String PARENTS_YEAR_OF_MARRIAGE = Birth.PARENTS_YEAR_OF_MARRIAGE;
 
     @LXP_SCALAR(type = LXPBaseType.STRING)
-    public static final String INFORMANT_DID_NOT_SIGN = "informant_did_not_sign";
+    public static final String PARENTS_PLACE_OF_MARRIAGE = Birth.PARENTS_PLACE_OF_MARRIAGE;
 
     @LXP_SCALAR(type = LXPBaseType.STRING)
-    public static final String INFORMANT = "informant";
+    public static final String INFORMANT_DID_NOT_SIGN = Birth.INFORMANT_DID_NOT_SIGN;
+
+    @LXP_SCALAR(type = LXPBaseType.STRING)
+    public static final String INFORMANT = Birth.INFORMANT;
 
     @LXP_SCALAR(type = LXPBaseType.STRING)
     public static final String FAMILY = "family";
-
 
     public BirthFamilyGT() {
 
