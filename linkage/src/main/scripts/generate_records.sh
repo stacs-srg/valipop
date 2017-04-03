5b893616002eeab018328d93702485bf407bdeb9
Özgür Akgün <ozgurakgun@gmail.com>
2017-04-03 13:34:34 +0100

    Remove linkage

--- linkage/src/main/scripts/generate_records.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-
-if [ -n "$1" ];
-then
-    export MAVEN_OPTS="-Xmx"$1"G"
-    echo Setting heap size: $1GB
-fi
-
-mvn exec:java -q -Dexec.cleanupDaemonThreads=false -Dexec.mainClass="uk.ac.standrews.cs.digitising_scotland.linkage.tools.GenerateEventRecordsFromDB" -e -Dexec.args="$2"
