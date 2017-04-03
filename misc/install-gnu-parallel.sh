a01da1469c4e484f378d8ed33f15ab9d89709489
Özgür Akgün <ozgurakgun@gmail.com>
2017-04-03 13:35:07 +0100

    Remove misc

--- misc/install-gnu-parallel.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-# installing parallel
-mkdir ~/tmp ; pushd ~/tmp
-wget ftp://ftp.gnu.org/gnu/parallel/parallel-latest.tar.bz2
-tar xvjf parallel-latest.tar.bz2
-pushd parallel-*
-./configure && make && sudo make install
-popd ; popd ; rm -rf ~/tmp
