a5f79c42539f806d6aa4c8af26a2fcf7e4b82c30
tomsdalton <tsd4@st-andrews.ac.uk>
2017-07-07 09:59:48 +0100

    Compile issues

--- validated/src/main/java/uk/ac/standrews/cs/digitising_scotland/verisim/utils/contingencyTables/extended/verisimContingencyTable/Table.java
+++ validated/src/main/java/uk/ac/standrews/cs/digitising_scotland/verisim/utils/contingencyTables/extended/verisimContingencyTable/Table.java
@@ -4,12 +4,16 @@ import uk.ac.standrews.cs.digitising_scotland.verisim.dateModel.Date;
 import uk.ac.standrews.cs.digitising_scotland.verisim.simulationEntities.person.IPersonExtended;
 import uk.ac.standrews.cs.digitising_scotland.verisim.utils.contingencyTables.extended.ContingencyTable;
 import uk.ac.standrews.cs.digitising_scotland.verisim.utils.contingencyTables.extended.Node;
+import uk.ac.standrews.cs.digitising_scotland.verisim.utils.contingencyTables.extended.verisimContingencyTable.enumerations.SourceType;
 
 
 /**
  * @author Tom Dalton (tsd4@st-andrews.ac.uk)
  */
-public class Table implements ContingencyTable {
+public class Table extends Node<String, SourceType> implements ContingencyTable {
+    public Table(String option, Node<?, String> parent) {
+        super(option, parent);
+    }
 
 
     // TODO write code
@@ -30,12 +34,12 @@ public class Table implements ContingencyTable {
     }
 
     @Override
-    public Node addChild(Object childOption, int initCount) {
+    public Node<SourceType, ?> addChild(SourceType childOption, int initCount) {
         return null;
     }
 
     @Override
-    public Node addChild(Object childOption) {
+    public Node<SourceType, ?> addChild(SourceType childOption) {
         return null;
     }
 
