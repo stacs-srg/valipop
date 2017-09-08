2957b3385eee7a0dece827f87f077e1b62f4872e
tomsdalton <tsd4@st-andrews.ac.uk>
2017-09-08 17:56:12 +0100

    Test inputs

---
+++ validated/src/main/resources/analysis-r/short-analysis.R
@@ -0,0 +1,219 @@
+calcP <- function(x) {
+  
+  # taken from MASS library - as used to calculate P values in summary(loglm)
+  if(x$df > 0L) {
+    return(1 - pchisq(x$lrt, x$df) )
+  } else { 
+    return(1)
+  }
+}
+
+deathAnalysis <- function(file) {
+  # Read in the data
+  #file <- "/Users/tsd4/OneDrive/cs/PhD/code/population-model/validated/src/main/resources/results/scot-sun-b/20170806-133506:129/tables/death-CT.csv"
+  data = read.csv(file, sep = ',', header = T)
+  
+  # Standardise the data
+  data$freq <- round(data$freq)
+  data <- data[which(data$freq != 0), ]
+  data <- data[which(data$Date >= 1855) , ]
+  data <- data[which(data$Date < 2015) , ]
+  
+  # Analysis
+  library("MASS")
+  
+  model = loglm(freq ~ Date * Sex * Age * Died, data = data)
+  
+  tryCatch(
+    {
+      model = step(model, direction = "both")  
+    } , error = function(err) {
+      p2 <- calcP(model)
+    }
+  )
+
+  p2 <- calcP(model)
+
+  return(c(p2, model$lrt, model$df))
+  
+}
+
+obAnalysis <- function(file, largestBirthLabel) {
+  #file <- "OneDrive/cs/PhD/code/population-model/validated/src/main/resources/results/scot-sun-c/20170806-145607:935/tables/ob-CT.csv"
+  data = read.csv(file, sep = ',', header = T)
+  
+  # Standardise the data
+  data$freq <- round(data$freq)
+  data <- data[which(data$freq != 0), ]
+  data <- data[which(data$Date >= 1855) , ]
+  data <- data[which(data$Date < 2015) , ]
+  data <- data[which(data$Age != "0to14"), ]
+  data <- data[which(data$Age != largestBirthLabel), ]
+  #data <- data[which(data$CIY == "YES"), ]
+  
+  # Analysis
+  library("MASS")
+  
+  model = loglm(freq ~ Age * NPCIAP * CIY * Date, data = data)
+  
+  tryCatch(
+    {
+      model = step(model, direction = "both")  
+    } , error = function(err) {
+      p2 <- calcP(model)
+    }
+  )
+  
+  p2 <- calcP(model)
+  
+  return(c(p2, model$lrt, model$df))
+  
+}
+
+mbAnalysis <- function(file, largestBirthLabel) {
+  data = read.csv(file, sep = ',', header = T)
+  
+  summary(data[which(data$Source == "SIM"),])
+  summary(data[which(data$Source == "STAT"),])
+  
+  # Standardise the data
+  data$freq <- round(data$freq)
+  data <- data[which(data$freq != 0), ]
+  data <- data[which(data$Date >= 1855) , ]
+  data <- data[which(data$Date < 2015) , ]
+  data <- data[which(data$Age != "0to14"), ]
+  data <- data[which(data$Age != largestBirthLabel), ]
+  
+  # Analysis
+  library("MASS")
+  
+  model = loglm(freq ~ Date * NCIY * Age, data = data)
+  
+  tryCatch(
+    {
+      model = step(model, direction = "both")  
+    } , error = function(err) {
+      p2 <- calcP(model)
+    }
+  )
+  
+  p2 <- calcP(model)
+  
+  return(c(p2, model$lrt, model$df))
+  
+}
+
+partAnalysis <- function(file) {
+  
+  #file <- "OneDrive/cs/PhD/code/population-model/validated/src/main/resources/results/scoty/20170805-182618:884/tables/part-CT.csv"
+  data = read.csv(file, sep = ',', header = T)
+  
+  # Standardise the data
+  data$freq <- round(data$freq)
+  data <- data[which(data$freq != 0), ]
+  data <- data[which(data$Date >= 1855) , ]
+  data <- data[which(data$Date < 2015) , ]
+  data <- data[which(data$NPA != "na") , ]
+  
+  # Analysis
+  library("MASS")
+  
+  model = loglm(freq ~ Date * NPA * Age, data = data)
+  
+  tryCatch(
+    {
+      model = step(model, direction = "both")  
+    } , error = function(err) {
+      p2 <- calcP(model)
+    }
+  )
+  
+  p2 <- calcP(model)
+  
+  return(c(p2, model$lrt, model$df))
+  
+}
+
+sepAnalysis <- function(file) {
+  
+  data = read.csv(file, sep = ',', header = TRUE)
+  
+  # Standardise the data
+  data$freq <- round(data$freq)
+  data <- data[which(data$freq != 0), ]
+  data <- data[which(data$Date >= 1855) , ]
+  data <- data[which(data$Date < 2015) , ]
+  data <- data[which(data$Separated != "NA") , ]
+  
+  # Analysis
+  library("MASS")
+  model = loglm(freq ~ Date + NCIP + Separated, data = data)
+  
+  tryCatch(
+    {
+      model = step(model, direction = "both")  
+    } , error = function(err) {
+      p2 <- calcP(model)
+    }
+  )
+  
+  p <- calcP(model)
+  
+  return(c(p, model$lrt, model$df))
+  
+}
+
+analysis <- function(outputFile, resultsDir, runFile, largestBirthLabel) {
+  
+  t <- proc.time()
+  
+  subpath <- paste(resultsDir, "/", runFile, "/tables", sep = "")
+  
+  death <- deathAnalysis(paste(subpath, "/death-CT.csv", sep = ""))
+  ob <- obAnalysis(paste(subpath, "/ob-CT.csv", sep = ""), largestBirthLabel)
+  mb <- mbAnalysis(paste(subpath, "/mb-CT.csv", sep = ""), largestBirthLabel)
+  part <- partAnalysis(paste(subpath, "/part-CT.csv", sep = ""))
+  sep <- sepAnalysis(paste(subpath, "/sep-CT.csv", sep = ""))
+  
+  tt <- proc.time() - t
+  
+  passed = FALSE
+  if(sum(death[1], ob[1], mb[1], part[1], sep[1]) == 5) {
+    passed = TRUE
+  }
+  
+  line = ""
+  line = paste(runFile, death[1], sep = ",")
+  line = paste(line, death[2], sep = ",")
+  line = paste(line, death[3], sep = ",")
+  line = paste(line, ob[1], sep = ",")
+  line = paste(line, ob[2], sep = ",")
+  line = paste(line, ob[3], sep = ",")
+  line = paste(line, mb[1], sep = ",")
+  line = paste(line, mb[2], sep = ",")
+  line = paste(line, mb[3], sep = ",")
+  line = paste(line, part[1], sep = ",")
+  line = paste(line, part[2], sep = ",")
+  line = paste(line, part[3], sep = ",")
+  line = paste(line, sep[1], sep = ",")
+  line = paste(line, sep[2], sep = ",")
+  line = paste(line, sep[3], sep = ",")
+  line = paste(line, passed, sep = ",")
+  line = paste(line, tt[3], sep = ",")
+  
+  if(!file.exists(outputFile)) {
+    headings = "RunFile,Death-P,Death-Dev,Death-DF,OB-P,OB-Dev,OB-DF,MB-P,MB-Dev,MB-DF,Part-P,Part-Dev,Part-DF,Sep-P,Sep-Dev,Sep-DF,Passed,Time-Taken"
+    
+    write(headings, file=outputFile, append=TRUE)
+  }
+  
+  write(line, file=outputFile, append=TRUE)
+  
+}
+
+outputFile <- commandArgs(TRUE)[1]
+resultsDir <- commandArgs(TRUE)[2]
+runFile <- commandArgs(TRUE)[3]
+largestBirthLabel <- commandArgs(TRUE)[4]
+
+analysis(outputFile, resultsDir, runFile, largestBirthLabel)
