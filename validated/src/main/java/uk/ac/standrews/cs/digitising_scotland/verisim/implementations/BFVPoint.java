a59a6a4af6c14ea01c9a53e669d4390824f06286
tomsdalton <tsd4@st-andrews.ac.uk>
2017-10-02 17:44:43 +0100

    Writing minima search code

---
+++ validated/src/main/java/uk/ac/standrews/cs/digitising_scotland/verisim/implementations/BFVPoint.java
@@ -0,0 +1,16 @@
+package uk.ac.standrews.cs.digitising_scotland.verisim.implementations;
+
+/**
+ * @author Tom Dalton (tsd4@st-andrews.ac.uk)
+ */
+public class BFVPoint {
+
+    double x_bf;
+    double y_v;
+
+    public BFVPoint(double x_bf, double y_v) {
+        this.x_bf = x_bf;
+        this.y_v = y_v;
+    }
+
+}
--- validated/src/main/java/uk/ac/standrews/cs/digitising_scotland/verisim/implementations/StatsException.java
+++ validated/src/main/java/uk/ac/standrews/cs/digitising_scotland/verisim/implementations/StatsException.java
@@ -4,4 +4,10 @@ package uk.ac.standrews.cs.digitising_scotland.verisim.implementations;
  * @author Tom Dalton (tsd4@st-andrews.ac.uk)
  */
 public class StatsException extends Exception {
+
+    public StatsException() {}
+
+    public StatsException(String m) {
+        super(m);
+    }
 }
