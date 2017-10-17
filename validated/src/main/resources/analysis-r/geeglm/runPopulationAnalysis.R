876d19c59ab9319566d42a0139f28d0a1f806940
tomsdalton <tsd4@st-andrews.ac.uk>
2017-10-17 17:32:10 +0100

    Fix to save location for analysis html file

--- validated/src/main/resources/analysis-r/geeglm/runPopulationAnalysis.R
+++ validated/src/main/resources/analysis-r/geeglm/runPopulationAnalysis.R
@@ -7,7 +7,7 @@ runAnalysis <- function(pathToRunDir, maxBirthAge, subTitle) {
   knitr::opts_chunk$set(fig.height=12, fig.width=16)
   knitr::knit2html(
     "src/main/resources/analysis-r/geeglm/analysis.Rhtml", 
-    output = paste(pathToRunDir, "analysis.html")
+    output = paste(pathToRunDir, "analysis.html", sep = "")
   )
 }
 
