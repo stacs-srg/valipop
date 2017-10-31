18ca1835af43f71bd39209a3086d190d79046521
tomsdalton <tsd4@st-andrews.ac.uk>
2017-10-31 17:14:47 +0000

    Updates to Analysis report stats

--- validated/src/main/java/uk/ac/standrews/cs/digitising_scotland/verisim/implementations/MinimaSearch.java
+++ validated/src/main/java/uk/ac/standrews/cs/digitising_scotland/verisim/implementations/MinimaSearch.java
@@ -14,6 +14,7 @@ import uk.ac.standrews.cs.digitising_scotland.verisim.utils.specialTypes.dateMod
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.nio.file.Paths;
 import java.util.*;
 
 /**
@@ -549,7 +550,7 @@ public class MinimaSearch {
 
     private static double getV(String pathOfTablesDir, int maxBirthingAge) throws StatsException, IOException {
 
-        String[] commands = {"Rscript", "src/main/resources/analysis-r/geeglm/dev-minima-search.R", pathOfTablesDir, String.valueOf(maxBirthingAge)};
+        String[] commands = {"Rscript", "src/main/resources/analysis-r/geeglm/dev-minima-search.R", System.getProperty("user.dir") + "/" + pathOfTablesDir, String.valueOf(maxBirthingAge)};
         ProcessBuilder pb = new ProcessBuilder(commands);
 
         Process proc;
