8689ebfa78c0c5eedbf535500344ec730773e49a
tomsdalton <tsd4@st-andrews.ac.uk>
2018-07-03 12:08:10 +0100

    Added R script ot install needed packages for analysis

---
+++ src/main/resources/valipop/analysis-r/geeglm/install-stats-packages.R
@@ -0,0 +1,8 @@
+install.packages("devtools")
+devtools::install_git("https://github.com/lindesaysh/MRSea.git")
+
+install.packages("MASS")
+install.packages("gridExtra")
+install.packages("geepack")
+install.packages("ggplot2")
+install.packages("mgcv")
