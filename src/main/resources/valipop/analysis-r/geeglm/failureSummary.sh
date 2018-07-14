085787697f6246125cdd37207c78a0c32619f9bb
tomsdalton <tsd4@st-andrews.ac.uk>
2018-07-14 11:58:20 +0100

    Fixed counter to work with parrallel stuff

--- src/main/resources/valipop/analysis-r/geeglm/failureSummary.sh
+++ src/main/resources/valipop/analysis-r/geeglm/failureSummary.sh
@@ -5,29 +5,38 @@ ANALYSIS=$1
 FAILURES=$2
 
 # Makes temp files
-touch ~/temp.txt
-touch ~/temp2.txt
+DATE=$(date +%s)
+
+DIRPATH="$(echo ~)/temp/$DATE"
+mkdir -p $DIRPATH
+
+FILEPATH1="$DIRPATH/temp.txt"
+FILEPATH2="$DIRPATH/temp2.txt"
+touch $FILEPATH1
+touch $FILEPATH2
+
+#touch ~/temp.txt
+#touch ~/temp2.txt
 
 # Gets titles
 grep -n "GEEGLM" $ANALYSIS > $FAILURES
 
 # Gets STATS interactions that are significant
-grep -n "STAT" $ANALYSIS > ~/temp.txt
-grep " \." ~/temp.txt >> $FAILURES
-grep "\*" ~/temp.txt >> $FAILURES
+grep -n "STAT" $ANALYSIS > $FILEPATH1
+grep " \." $FILEPATH1 >> $FAILURES
+grep "\*" $FILEPATH1 >> $FAILURES
 
 # Gets any error messages
 grep -n "Error" $ANALYSIS >> $FAILURES
 
 # Sorts by line number from origonal ANALYSIS file
-sort -n -t: -k1 $FAILURES > ~/temp.txt
+sort -n -t: -k1 $FAILURES > $FILEPATH1
 
 # Strips HTML tags if present
-sed -e 's/<[^>]*>//g' ~/temp.txt > ~/temp2.txt
+sed -e 's/<[^>]*>//g' $FILEPATH1 > $FILEPATH2
 
 # Strips line numbers
-sed 's/ *[0-9]*.//' ~/temp2.txt > $FAILURES
+sed 's/ *[0-9]*.//' $FILEPATH1 > $FAILURES
 
 # Removes temp files
-rm -rf ~/temp.txt
-rm -rf ~/temp2.txt
+rm -rf $DIRPATH
