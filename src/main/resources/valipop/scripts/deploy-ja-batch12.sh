6a9e119f769cfb4867187f061be8a986db529e71
tomsdalton <tsd4@st-andrews.ac.uk>
2018-09-20 17:33:37 +0100

    JA deploy scripts fixed

--- src/main/resources/valipop/scripts/deploy-ja-batch12.sh
+++ src/main/resources/valipop/scripts/deploy-ja-batch12.sh
@@ -6,5 +6,5 @@ TD_NODE_IN_USE=`echo $TD_LOAD_AVG'>'0.5 | bc -l`
 
 if [ $TD_NODE_IN_USE -eq 0 ]; then
 	cd population-model
-	nohup sh src/main/resources/valipop/scripts/run-factor-search.sh 14 src/main/resources/valipop/inputs/proxy-scotland-population-JA/ 10000 ja-batch12 1 0.1,0.2,0.4,0.5,0.7,0.8,0.9 0.1,0.2,0.4,0.5,0.7,0.8,0.9 /cs/tmp/tsd4/results/ > ja-batch12-`hostname`.txt
+	nohup sh src/main/resources/valipop/scripts/run-factor-search.sh 14 src/main/resources/valipop/inputs/proxy-scotland-population-JA/ 10000 ja-batch12 4 0.1,0.2,0.4,0.5,0.7,0.8,0.9 0.1,0.2,0.4,0.5,0.7,0.8,0.9 /cs/tmp/tsd4/results/ > ja-batch12-`hostname`.txt
 fi
