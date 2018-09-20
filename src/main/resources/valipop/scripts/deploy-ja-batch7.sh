f3c1d2c52bfeac7639214ac450dc2f8a623c8404
tomsdalton <tsd4@st-andrews.ac.uk>
2018-09-20 10:59:08 +0100

    JA deploy scripts

---
+++ src/main/resources/valipop/scripts/deploy-ja-batch7.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+TD_LOAD_AVG=`uptime | sed 's/.*load average: //' | awk -F\, '{print $1}'`
+TD_NODE_IN_USE=`echo $TD_LOAD_AVG'>'0.5 | bc -l`
+
+
+if [ $TD_NODE_IN_USE -eq 0 ]; then
+	cd population-model
+	nohup sh src/main/resources/valipop/scripts/run-factor-search.sh 14 src/main/resources/valipop/inputs/proxy-scotland-population-JA/ 40000 ja-batch7 1 0.1,0.2,0.4,0.5,0.7,0.8,0.9 0.0,0.3,0.6,1.0 /cs/tmp/tsd4/results/ > ja-batch7-`hostname`.txt
+fi
