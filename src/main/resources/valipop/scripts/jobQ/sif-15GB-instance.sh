124593b72a0f1753863b22386fc4eb394cb4d141
tomsdalton <tsd4@st-andrews.ac.uk>
2019-06-19 15:28:00 +0100

    Update to instance parameters and further deadlock logging

--- src/main/resources/valipop/scripts/jobQ/sif-15GB-instance.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-
-cd population-model
-nohup sh src/main/resources/valipop/scripts/jobQ/start-job-q-instance.sh src/main/resources/valipop/scripts/jobQ/status-sif.txt src/main/resources/valipop/scripts/jobQ/job-q-clusters.csv 15 1 1.5 > runs/job-run-`hostname`.txt
