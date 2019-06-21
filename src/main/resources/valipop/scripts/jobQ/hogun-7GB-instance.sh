fd770c0c0a21e357ade74931cebbb2dc19dc396f
tomsdalton <tsd4@st-andrews.ac.uk>
2019-06-21 17:11:11 +0100

    Scripts for additional instances

---
+++ src/main/resources/valipop/scripts/jobQ/hogun-7GB-instance.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+cd population-model
+nohup sh src/main/resources/valipop/scripts/jobQ/start-job-q-instance.sh src/main/resources/valipop/scripts/jobQ/status-hogun.txt src/main/resources/valipop/scripts/jobQ/job-q-clusters.csv 7 1 1.5 > runs/job-run-`hostname`.txt
