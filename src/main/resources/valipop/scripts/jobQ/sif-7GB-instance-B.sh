7d5b365efc9b5edc9d3e36c7cf4c7d155eadf365
tomsdalton <tsd4@st-andrews.ac.uk>
2019-07-01 15:13:54 +0100

    Further instance scripts

---
+++ src/main/resources/valipop/scripts/jobQ/sif-7GB-instance-B.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+cd population-model
+nohup sh src/main/resources/valipop/scripts/jobQ/start-job-q-instance.sh src/main/resources/valipop/scripts/jobQ/status-sif.txt src/main/resources/valipop/scripts/jobQ/job-q-clusters.csv 7 1 5.0 > runs/job-run-`hostname`-B.txt
