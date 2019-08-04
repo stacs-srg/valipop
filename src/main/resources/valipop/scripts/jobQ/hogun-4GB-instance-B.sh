3779d88ee115102520f8e292d8661a22026f19cb
tomsdalton <tsd4@st-andrews.ac.uk>
2019-08-04 02:26:52 +0100

    New cluster deploy scripts

---
+++ src/main/resources/valipop/scripts/jobQ/hogun-4GB-instance-B.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+cd population-model
+nohup sh src/main/resources/valipop/scripts/jobQ/start-job-q-instance.sh src/main/resources/valipop/scripts/jobQ/status-hogun.txt src/main/resources/valipop/scripts/jobQ/job-q-clusters.csv 4 1 5.0 > runs/job-run-`hostname`-B.txt
