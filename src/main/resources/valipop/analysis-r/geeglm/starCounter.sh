d675ed41ed1029166a53f2e5603f82d6df8f8064
Tom Dalton <tdalton@adobe.com>
2020-09-21 22:14:24 +0100

    Counter update for errors and new job q

--- src/main/resources/valipop/analysis-r/geeglm/starCounter.sh
+++ src/main/resources/valipop/analysis-r/geeglm/starCounter.sh
@@ -8,6 +8,8 @@ TWO=$(fgrep -o \*\* $FAILURES | wc -l)
 ONE=$(fgrep -o \* $FAILURES | wc -l)
 DOTS=$(fgrep -o " ." $FAILURES | wc -c)
 
+ERRORS=$(fgrep -o " error" $FAILURES | wc -c)
+
 # This is me trying to divide by using bash...
 T=3
 DOT=`expr $DOTS / $T`
@@ -19,4 +21,9 @@ ONE=`expr $ONE - $THREE \* 3 - $TWO \* 2`
 # Calculates optomisation value
 # TOTAL=`expr $THREE \* 4 + $TWO \* 3 + $ONE \* 2 + $DOT`
 TOTAL=`expr $THREE \* 3 + $TWO \* 2 + $ONE`
-echo $TOTAL
+
+if [ "$ERRORS" -eq "0"]; then
+  echo $TOTAL
+else
+  echo -1
+fi
