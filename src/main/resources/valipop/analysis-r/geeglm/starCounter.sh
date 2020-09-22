3cbc06497399c391b93f66ea42060a08842452a7
Tom Dalton <tdalton@adobe.com>
2020-09-22 07:38:40 +0100

    Star counter script fix

--- src/main/resources/valipop/analysis-r/geeglm/starCounter.sh
+++ src/main/resources/valipop/analysis-r/geeglm/starCounter.sh
@@ -22,7 +22,7 @@ ONE=`expr $ONE - $THREE \* 3 - $TWO \* 2`
 # TOTAL=`expr $THREE \* 4 + $TWO \* 3 + $ONE \* 2 + $DOT`
 TOTAL=`expr $THREE \* 3 + $TWO \* 2 + $ONE`
 
-if [ "$ERRORS" -eq "0"]; then
+if [ "$ERRORS" -eq "0" ]; then
   echo $TOTAL
 else
   echo -1
